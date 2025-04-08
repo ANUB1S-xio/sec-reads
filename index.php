c0a7f35bdbad5d74661ab7b4ab64de17386ad711
Tanner L <140106289+ANUB1S-xio@users.noreply.github.com>
2025-04-08 10:06:49 -0500

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html>
 <head>
     <title>Sec-Reads - Home</title>
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
