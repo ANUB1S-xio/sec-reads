667f349527a60d2499904a4b92fd80930b42c0ad
Tanner L <140106289+ANUB1S-xio@users.noreply.github.com>
2025-04-08 10:09:33 -0500

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -9,6 +9,7 @@ if (!isset($_SESSION['user_id'])) {
 <html>
 <head>
     <title>Checkout</title>
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
