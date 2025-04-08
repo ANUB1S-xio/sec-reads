2b418fb8add94bcf051209722aaffd2890548a88
Tanner L <140106289+ANUB1S-xio@users.noreply.github.com>
2025-04-08 10:09:03 -0500

    Update cart.php

--- cart.php
+++ cart.php
@@ -6,6 +6,7 @@ $cart = $_SESSION['cart'] ?? [];
 <html>
 <head>
     <title>Your Cart</title>
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
